953d100879144d7961a15faab424dea542c6b7f0
GHMH5 <fikaegyptus@openmailbox.org>
2019-11-16 16:02:36 -0500

    First commit

---
+++ Akif_SQL.sql
@@ -0,0 +1,6 @@
+SHOW Databases; 
+use books;
+
+INSERT INTO books (ObjectID, Title, Author, BookYear) VALUES(4, "TheOldManAndTheSea", "Earnest Hemingway", '1952');
+INSERT INTO books (ObjectID, Title, Author, BookYear) VALUES(5, "Anna Karenina", "Leo Tolstoy", '1873');
+INSERT INTO books (ObjectID, Title, Author, BookYear) VALUES(6, "ThreeMenInABoatbooks", "Jerome K. Jerome", '1889');
